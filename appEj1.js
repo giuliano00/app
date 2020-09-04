a927d5268197ff5786f9a5b3e49920e087fe53d5
giuliano00 <giuliano.botto@davinci.edu.ar>
2020-09-04 11:59:12 -0300

    hasta el 12 falta 13

--- appEj1.js
+++ appEj1.js
@@ -115,3 +115,135 @@ contiene=[7,8,9]
 const result= contiene.filter(contiene=>contiene==8);
 console.log(result);
 */
+//--------------------------------------------------------------------------------------------------------------
+
+//11- Crea una clase Persona que tenga los atributos nombre y edad. 
+//Además agregale un método presentarse() que como resultado imprima un mensaje diciendo su nombre y edad.
+/*
+class Persona
+{
+
+constructor(nombre , edad)
+ {
+  this.nombre=nombre;
+  this.edad=edad;
+ }
+ Presentarse()
+ {
+   return this.nombre+" "+this.edad+" "+"este es mi nobre y edad";
+ }
+
+}
+
+var persona = new Persona("giuliano",20);
+console.log(persona.Presentarse());
+*/
+//12- Crea una clase Estudiante (que herede de Persona) que tenga un atributo profesor y tenga dos métodos.
+//El método estudiando() que imprime el mensaje “Estudiando con ” y el nombre del profesor con el que se encuentra estudiando.
+//El método setProfesor(profesor) que setea con qué profesor se encuentra estudiando.
+/*
+class Persona
+{
+
+constructor(nombre , edad)
+ {
+  this.nombre=nombre;
+  this.edad=edad;
+ }
+ Presentarse()
+ {
+   return this.nombre+" "+this.edad+" "+"este es mi nobre y edad";
+ }
+
+}
+
+class Estidiante extends Persona
+{
+  constructor (nombre,edad,profesor)
+  {
+super(nombre,edad)
+this.profesor=profesor;
+  }
+
+estudiando()
+  {
+return "Estudiando con"+" "+this.profesor;
+  }
+
+  setProfesor(profesor)
+  {
+    this.profesor=profesor;
+  }
+
+}
+
+var estidiante = new Estidiante("giuliano",20,"Christian Dario Nievas");
+console.log(estidiante.Presentarse());
+console.log(estidiante.estudiando());
+*/
+
+//13- Crea una clase Profesor (que herede de Persona) que tenga un atributo estudiantes con un array de Estudiante.
+// Además debe tener dos métodos enseñando() que imprima por consola todos sus estudiantes a los cuales les está enseñando y
+//el método addEstudiante(unEstudiante) que agregue a ese estudiante a la lista.
+// Además este método es responsable de manejar de agregar al objeto profesor como profesor del estudiante (manejo de doble referencia)
+
+
+class Persona
+{
+
+constructor(nombre , edad)
+ {
+  this.nombre=nombre;
+  this.edad=edad;
+ }
+ Presentarse()
+ {
+   return this.nombre+" "+this.edad+" "+"este es mi nobre y edad";
+ }
+
+}
+
+class Estudiante extends Persona
+{
+  constructor (nombre,edad,profesor)
+  {
+super(nombre,edad)
+this.profesor=profesor;
+  }
+
+estudiando()
+  {
+return "Estudiando con"+" "+this.profesor;
+  }
+
+  setProfesor(profesor)
+  {
+    this.profesor=profesor;
+  }
+
+}
+
+class Profesor extends Perssona
+{
+  constructor(nombre ,edad,Estudiante)
+  {
+    super (nombre, edad);
+
+  }
+
+  addEstudiante (estudiante)
+  {
+
+  }
+
+  var arrayAlumnos=
+  [
+    new Estudiante("giuliano",20),
+    new Estudiante("federico",34),
+    new Estudiante("giselle",36),
+  ];
+
+
+
+
+}
