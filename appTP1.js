e31636b3ab01a492193e77c219ee82fa2be143b7
giuliano00 <giuliano.botto@davinci.edu.ar>
2020-09-23 15:55:50 -0300

    f13

--- appTP1.js
+++ appTP1.js
@@ -9,10 +9,13 @@ console.log(x);
 
 //2- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla los doce nombres utilizando la función console.log()
 /*
-var y =["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
-console.log(y);
-*/
+var M =["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
 
+for(var i=0;i<M.length;i++){
+
+console.log(M[i]);
+}
+*/
 //--------------------------------------------------------------------------------------------------------------
 
 //3- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar.
@@ -33,48 +36,48 @@ console.log(parImpar(2));
 
 //4- Define una función que dado un array de números nos dice si alguno es par.
 /*
-algunoEsPar = [1,2,3]
-
-algunoEsPar.forEach(myFunction)
-
-function myFunction(item, index, arr) {
-  arr[index] = item % 2 == 0;
-  return true
+function algunoEsPar (arrnum){
+if (Array.isArray(arrnum)){
+return arrnum.some (Element=>Element%2==0);
+}else{
+  console.log("no cuenta como arr");
 }
-*/
+}
+console.log(algunoEsPar([1,2,3]));
 
+*/
 //--------------------------------------------------------------------------------------------------------------
 
 //5- Define la función aprobó, que dada la lista de las notas de un alumno devuelve si el alumno aprobó.
 // Un alumno aprobó si todas sus notas son mayores o iguales a 4
 /*
-const aprobo =[
-{notas1:8,notas2:6,notas3:2,notas4:4}
-]
+function aprobo (nota){
+  
+  var notaaprobado = 4;
 
-const check=aprobo.every(item=>{
-  return item.notas1>=4 && item.notas2>=4 && item.notas3>=4 && item.notas4>=4
-})
-
-console.log(check)
+  return nota.every(Element=>Element>=notaaprobado)
+}
+console.log(aprobo([8,6,2,4]));
 */
-
 //--------------------------------------------------------------------------------------------------------------
 
 //6- Define la función quienesAprobaron, que dada la información de un curso devuelve la información de los alumnos que aprobaron.
 // Podés usar la función que hiciste en el ejercicio anterior.
 /*
-const quienesAprobaron =[
-  {nota1:8,nota2:6,nota3:2,nota4:4},
-  {nota1:7,nota2:9,nota3:4,nota4:5},
-  {nota1:6,nota2:2,nota3:4,nota4:2},
-  {nota1:9,nota2:6,nota3:7,nota4:10}];
+function aprobo (nota){
+  
+  var notaaprobado = 4;
 
-var result=quienesAprobaron.filter(function(quienesAprobaron){
-  return quienesAprobaron.nota1>=4 && quienesAprobaron.nota2>=4 && quienesAprobaron.nota3>=4 && quienesAprobaron.nota4>=4;
-})
+  return nota.every(Element=>Element>=notaaprobado)
 
-console.log(result);
+}
+function quienesAprobaron (nota){
+
+  return nota.filter(notas=>aprobo(notas))
+  
+} 
+
+ console.log(quienesAprobaron([[8,6,2,4],[7,9,4,5],[6,2,4,2],[9,6,7,10]]));
 
 */
 
@@ -82,39 +85,63 @@ console.log(result);
 
 //7- Define la función hayAlgunNegativo, que dada una lista de números nos dice si hay algún negativo
 /*
- hayAlgunNegativo =[2,-3,9]
- const even=(element)=> element <0;
-console.log(hayAlgunNegativo.some(even));
+function  hayAlgunNegativo (arr){
+ return arr.some (Element=>Element<0);
+}
+console.log( hayAlgunNegativo([2,-3,9]));
 */
-
 //--------------------------------------------------------------------------------------------------------------
 
 //8- Escribi una función cuantosCumplen que dada una condición y una lista, diga cuantos elementos la cumplen
 /*
-cuantosCumplen=[7,9,25,42]
-const result= cuantosCumplen.filter(cuantosCumplen=>cuantosCumplen% 2 == 0) ;
-console.log(result);
+function even(Element){
+
+  return Element%2===0;
+
+}
+function cuantosCumplen(fun,array){
+
+  var arr= array.filter(Element => fun(Element));
+
+  return arr.length;
+
+}
+
+console.log(cuantosCumplen(even,[7,9,25,42]));
+
 */
-
-
 //--------------------------------------------------------------------------------------------------------------
 
 //9- Escribí una función rechazar, que dada una condición y un array, devuelva un array con los elementos que no la cumplen
 /*
-rechazar=[7,9,87,42]
-const result= rechazar.filter(rechazar=>rechazar% 2 == 1);
-console.log(result);
-*/
 
+function even(Element){
+
+  return Element%2===0;
+
+}
+
+function rechazar(fun,arr){
+
+var arrqnocumplen = arr.filter(Element=>!fun(Element))
+
+return arrqnocumplen;
+}
+
+console.log(rechazar(even,[7,9,87,42]));
+*/
 //--------------------------------------------------------------------------------------------------------------
 
 //10- Define la función contiene que dado un elemento y una lista, nos diga si la lista contiene al elemento
-/*
-contiene=[7,8,9]
 
-const result= contiene.filter(contiene=>contiene==8);
-console.log(result);
-*/
+function contiene (num,arr) {
+
+  return arr.some (Element=> Element ==num);
+
+}
+
+console.log(contiene(8,[7,8,9]));
+
 //--------------------------------------------------------------------------------------------------------------
 
 //11- Crea una clase Persona que tenga los atributos nombre y edad. 
@@ -203,6 +230,9 @@ constructor(nombre , edad)
 
 }
 
+
+
+
 class Estudiante extends Persona
 {
   constructor (nombre,edad,profesor)
@@ -222,6 +252,9 @@ return "Estudiando con"+" "+this.profesor;
   }
 
 }
+
+
+
 
 class Profesor extends Perssona
 {
